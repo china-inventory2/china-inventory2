c8d6c7c48103716168352791034607a8bbc11a41
ミント <minto@mintonoMacBook-Pro.local>
2020-03-07 21:02:24 +0900

    one_view_model_request_spec

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'ログインしました。'
+      if user.inventory_manager_flg == true || user.admin == true
+        redirect_to root_url
+      else user.reserch_user_flg == true
+        redirect_to root_url
+      end
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています。'
+      render 'static_pages/top'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_path
+  end
+end
