e5e8a0cf15842d04835d44b50c7c34590a45063c
ミント <minto@mintonoMacBook-Pro.local>
2020-03-20 19:46:15 +0900

    fix_rubocop.yml

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
-
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+environment.plugins.set(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  }),
+);
 module.exports = environment
